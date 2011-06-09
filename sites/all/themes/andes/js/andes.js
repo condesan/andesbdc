4ca498d9e581fa5d8923b9ac371969f7fb25f56d
= <=>
2011-06-09 14:24:30 -0500

    Primer envio

---
+++ sites/all/themes/andes/js/andes.js
@@ -0,0 +1,17 @@
+(function($){
+$(document).ready(function(){
+
+
+
+$('#header .form-submit').attr('value', '');
+$('#block-menu-menu-proyectos h2.title').html('');
+
+$('#block-menu-menu-mquienessomos h2.title').html('');
+$('.section-investigadores h1').html('');
+
+
+
+});
+})(jQuery);
+
+
