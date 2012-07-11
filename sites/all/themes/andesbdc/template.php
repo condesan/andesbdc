b001b661a138a5b47e2d2053e140f261575229dc
Adam <a.sanchez75@gmail.com>
2012-07-11 09:42:20 -0500

    implementation of titles using hook_preprocess in omega

--- sites/all/themes/andesbdc/template.php
+++ sites/all/themes/andesbdc/template.php
@@ -28,4 +28,32 @@ function andesbdc_menu_link(array $variables) {
   return '<li' . drupal_attributes($element['#attributes']) . '>' . $output . $sub_menu . "</li>\n";
 }
 
+/*
+ * muestra franja
+ */ 
+function _andesbdc_display_fringe($content) {
+  $output  = "<div id=\"introduction\"><div id=\"block-block-7\" class=\"container-12 clearfix\"><div class=\"grid-12\">";
+  $output .= render($content);
+  $output .= "</div></div></div>";
+ return $output;
+}
 
+/**
+ * Implements hook_preprocess_zone().
+ */
+function andesbdc_alpha_preprocess_zone(&$vars) {
+  if (alpha_library_active('omega_equalheights')) {
+    if (!empty($vars['elements']['#data']['equal_height_container'])) {
+      $vars['content_attributes_array']['class'][] = 'equal-height-container';
+    }
+  }
+  if ($vars['elements']['#zone'] == 'header') {
+    if (drupal_is_front_page()) {
+      $block = module_invoke('block', 'block_view', '7');
+      $vars['content_fringe'] = _andesbdc_display_fringe($block['content']);
+    }
+    else {
+      $vars['content_fringe'] = _andesbdc_display_fringe(drupal_get_title());  
+      }
+  }  
+}
